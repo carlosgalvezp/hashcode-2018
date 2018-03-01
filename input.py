8430127438b474c3ebc97d80828b41ba64e11c91
Carlos Galvez <carlosgalvezp@gmail.com>
2018-03-01 19:04:13 +0100

    Parse input

---
+++ input.py
@@ -0,0 +1,47 @@
+class InputData(object):
+    def __init__(self):
+        self.rows = 0
+        self.columns = 0
+        self.vehicles = 0
+        self.rides = []
+        self.bonus = 0
+        self.steps = 0
+
+class Ride(object):
+    def __init__(self):
+        self.start = [0,0]
+        self.end = [0,0]
+        self.earliest_start = 0
+        self.latest_finish = 0
+
+
+# Vehicle drives one unit per step
+# vehicle can start new ride in the same step as previous ride finished,
+# if ride starts in the same intersection as previous ride finished
+def parse_input(input_file):
+    input_data = InputData()
+
+    with open(input_file, 'r') as f:
+        first_line = f.readline()
+        tokens = first_line.split(' ')
+
+        input_data.rows = int(tokens[0])
+        input_data.cols = int(tokens[1])
+        input_data.vehicles = int(tokens[2])
+        n_rides = int(tokens[3])
+        input_data.bonus = int(tokens[4])
+        input_data.steps = int(tokens[5])
+
+        for _ in range(n_rides):
+            ride = Ride()
+
+            tokens_ride = f.readline().split(' ')
+
+            ride.start = [int(tokens_ride[0]), int(tokens_ride[1])]
+            ride.end = [int(tokens_ride[2]), int(tokens_ride[3])]
+            ride.earliest_start = int(tokens_ride[4])
+            ride.latest_finish = int(tokens_ride[5])
+
+            input_data.rides.append(ride)
+
+    return input_data
--- solution.py
+++ solution.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+import sys
+import os
+
+from input import parse_input
+
+
+def process_data(data):
+    print('processing data')
+    return None
+
+def produce_output(output):
+    print('Hello World!')
+
+
+def main():
+    data = parse_input(os.path.join('/home/cgalvezd/git/hashcode-2018', 'data','a_example.in'))
+    #output = process_data(data)
+    #produce_output(output)
 
 if __name__ == '__main__':
-    print('Hello World!')
+    main()
